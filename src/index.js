95ce22e8cb9dc81cc53ad639ee6c6ee7d95dc2ae
Syed Dawar Alvi <46403138+DawarAlvi@users.noreply.github.com>
2023-12-18 13:38:31 +0530

    utils

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
-import dotenv from "dotenv"
-import connectDB from "./db/index.js"
-dotenv.config({path: "./.env"});
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+dotenv.config({ path: "./.env" });
 
-connectDB();
-
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`server is running at port ${process.env.PORT || 8000}`);
+    });
+  })
+  .catch((error) => {
+    console.log("Mong DB connection failed!!", error);
+  });
